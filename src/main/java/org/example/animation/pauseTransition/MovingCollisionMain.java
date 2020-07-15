538ce1b6d26abe916a6160876bc4925cf56243da
andreas-repo <andreas.mayer@gepardec.com>
2020-07-15 14:40:41 +0200

    -- add Moving Transition Example -- add Moving Pause Collision

---
+++ src/main/java/org/example/animation/pauseTransition/MovingCollisionMain.java
@@ -0,0 +1,7 @@
+package org.example.animation.pauseTransition;
+
+public class MovingCollisionMain {
+    public static void main(String[] args) {
+        MovingPauseTransitionExample.main(args);
+    }
+}
