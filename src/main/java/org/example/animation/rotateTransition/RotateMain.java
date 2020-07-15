ad20b0e3bb36b27d2ac5c4e7f11c50d89c5aac42
andreas-repo <andreas.mayer@gepardec.com>
2020-07-15 13:01:20 +0200

    -- scale transition --rotate transition

---
+++ src/main/java/org/example/animation/rotateTransition/RotateMain.java
@@ -0,0 +1,7 @@
+package org.example.animation.rotateTransition;
+
+public class RotateMain {
+    public static void main(String[] args) {
+        RotateTransitionExample.main(args);
+    }
+}
---
+++ src/main/java/org/example/animation/scaleTransition/ScaleMain.java
@@ -0,0 +1,7 @@
+package org.example.animation.scaleTransition;
+
+public class ScaleMain {
+    public static void main(String[] args) {
+        ScaleTransitionExample.main(args);
+    }
+}
